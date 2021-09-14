3edd589416027f1c8367fd48d7db2e18fbb9e721
lhb0269 <lhb0269@gmail.com>
2021-09-14 19:44:57 +0900

    DRILL#05완료  https://www.youtube.com/watch?v=_GkbxaqFNdE&ab_channel=%EC%9D%B4%ED%95%9C%EB%B9%9B

---
+++ 05/DRILL#05.py
@@ -0,0 +1,34 @@
+import turtle
+
+def move_up():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+def movea_left():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+
+def move_down():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+
+def moved_right():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+def restart():
+    turtle.home()
+    turtle.clear()
+
+
+turtle.shape('turtle')
+
+turtle.onkey(move_up,'w')
+turtle.onkey(movea_left,'a')
+turtle.onkey(move_down,'s')
+turtle.onkey(moved_right,'d')
+turtle.onkey(restart,'Escape')
+turtle.listen()
